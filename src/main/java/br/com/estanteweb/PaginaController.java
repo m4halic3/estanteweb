a3a3ff23d4bd080997480102406847c16da03d85
m4halic3 <mariana.alice667@gmail.com>
2026-08-14 15:52:15 -0300

    Corrigindo comentários referentes à missão 2

--- src/main/java/br/com/estanteweb/PaginaController.java
+++ src/main/java/br/com/estanteweb/PaginaController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaginaController {
-    // Missão 1.1
+    // Missão 2.1
     @GetMapping("/")
     public String boasVindas() {
         return "EstanteWeb: Livros usados com história";
     }
-    //Missão 1.2
+    //Missão 2.2
     @GetMapping("/sobre")
     public String sobre (){
         return "Sebo de bairro especializado em livros usados, com um acervo cuidadosamente selecionado";
     }
-    //Missão 1.3
+    //Missão 2.3
     //@GetMapping("/sobre")
     //public String sobreDuplicado() {
         //return "Outra descrição da loja";
