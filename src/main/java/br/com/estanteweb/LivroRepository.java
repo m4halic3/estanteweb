2851d86699f9fa41580f7e044a83418351cbcd9c
m4halic3 <mariana.alice667@gmail.com>
2026-08-14 16:08:35 -0300

    Adicionando maus um livro dentro do acervo

--- src/main/java/br/com/estanteweb/LivroRepository.java
+++ src/main/java/br/com/estanteweb/LivroRepository.java
@@ -13,6 +13,7 @@ public class LivroRepository {
         acervo.add(new Livro("O Pequeno Príncipe", "Antoine de Saint-Exupéry", 1943, 19.90));
         acervo.add(new Livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1954, 49.90));
         acervo.add(new Livro("O Hobbit", "J.R.R. Tolkien", 1937, 39.90));
+        acervo.add(new Livro("O <b>Grande</b> Gatsby", "F. Scott Fitzgerald", 1925, 34.90));
     }
 
     public List<Livro> listarTodos() {
